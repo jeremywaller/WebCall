713659a35402cd463ce31805cac1cb86f42494ab
Jeremy Waller <jeremywaller@gmail.com>
2017-03-29 15:35:24 -0500

    Changed serializer for json to Newtonsoft

---
+++ WebCall/Interfaces/IJsonSerializer.cs
@@ -0,0 +1,10 @@
+﻿using RestSharp.Deserializers;
+using RestSharp.Serializers;
+
+namespace WebCall.Interfaces
+{
+    public interface IJsonSerializer : ISerializer, IDeserializer
+    {
+        
+    }
+}
---
+++ WebCall/Interfaces/NewtonsoftJsonSerializer.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace WebCall.Interfaces
+{
+    public class NewtonsoftJsonSerializer : IJsonSerializer
+    {
+        // Borrowed from http://bytefish.de/blog/restsharp_custom_json_serializer/
+
+        private readonly JsonSerializer _serializer;
+
+        public NewtonsoftJsonSerializer(JsonSerializer serializer)
+        {
+            _serializer = serializer;
+        }
+
+        public string ContentType
+        {
+            get { return "application/json"; } // Probably used for Serialization?
+            set { }
+        }
+
+        public string DateFormat { get; set; }
+
+        public string Namespace { get; set; }
+
+        public string RootElement { get; set; }
+
+        public string Serialize(object obj)
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                using (var jsonTextWriter = new JsonTextWriter(stringWriter))
+                {
+                    _serializer.Serialize(jsonTextWriter, obj);
+
+                    return stringWriter.ToString();
+                }
+            }
+        }
+
+        public T Deserialize<T>(RestSharp.IRestResponse response)
+        {
+            var content = response.Content;
+
+            using (var stringReader = new StringReader(content))
+            {
+                using (var jsonTextReader = new JsonTextReader(stringReader))
+                {
+                    return _serializer.Deserialize<T>(jsonTextReader);
+                }
+            }
+        }
+
+        public static NewtonsoftJsonSerializer Default
+        {
+            get
+            {
+                return new NewtonsoftJsonSerializer(new JsonSerializer()
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                });
+            }
+        }
+    }
+}
